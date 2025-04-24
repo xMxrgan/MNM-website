5c03a30f6f48953240a35cf1a84e7b377dfa898c
Morgan Picariello <Morgan@MacBook-Air-di-Maria-Vittoria.local>
2025-04-24 10:45:35 +0200

    Changed activities

--- src/components/activityCard.tsx
+++ src/components/activityCard.tsx
@@ -2,60 +2,58 @@ import { useState, useRef, useEffect } from "react";
 import "../Style.css";
 
 const ActivityCard = ({
-    imageSrc,
-    title,
-    description,
-    dateAndTime,
+  imageSrc,
+  title,
+  description,
+  dateAndTimeConcamarise,
+  dateAndTimeBovolone,
 }: {
-    imageSrc: string;
-    title: string;
-    description: string;
-    dateAndTime: string;
+  imageSrc: string;
+  title: string;
+  description: string;
+  dateAndTimeConcamarise?: string;
+  dateAndTimeBovolone?: string;
 }) => {
-    const [isExpanded, setIsExpanded] = useState(false);
-    const cardRef = useRef<HTMLFormElement>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const cardRef = useRef<HTMLFormElement>(null);
 
-    const toggleCard = () => {
-        setIsExpanded((prev) => !prev);
+  const toggleCard = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (cardRef.current && !cardRef.current.contains(event.target as Node)) {
+        setIsExpanded(false);
+      }
     };
 
-    useEffect(() => {
-        const handleClickOutside = (event: MouseEvent) => {
-            if (
-                cardRef.current &&
-                !cardRef.current.contains(event.target as Node)
-            ) {
-                setIsExpanded(false);
-            }
-        };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
-
-    return (
-        <form
-            ref={cardRef}
-            className={`activity-card ${isExpanded ? "expanded" : ""}`}
-            onClick={toggleCard}
-        >
-            <img src={imageSrc} alt={title} className="activity-card-img" />
-            <div className="activity-card-content">
-                <h2 className="activity-card-title">{title}</h2>
-                <h3 className="activity-card-time">{dateAndTime}</h3>
-                <span className="activity-card-icon">⬇️</span>
-            </div>
-            <div
-                className={`activity-card-description ${
-                    isExpanded ? "visible" : ""
-                }`}
-            >
-                <p>{description}</p>
-            </div>
-        </form>
-    );
+  return (
+    <form
+      ref={cardRef}
+      className={`activity-card ${isExpanded ? "expanded" : ""}`}
+      onClick={toggleCard}
+    >
+      <img src={imageSrc} alt={title} className="activity-card-img" />
+      <div className="activity-card-content">
+        <h2 className="activity-card-title">{title}</h2>
+        <h3 className="activity-card-time">{dateAndTimeConcamarise}</h3>
+        <h3 className="activity-card-time">{dateAndTimeBovolone}</h3>
+        <span className="activity-card-icon">⬇️</span>
+      </div>
+      <div
+        className={`activity-card-description ${isExpanded ? "visible" : ""}`}
+      >
+        <p>{description}</p>
+      </div>
+    </form>
+  );
 };
 
 export default ActivityCard;
--- src/views/activities.tsx
+++ src/views/activities.tsx
@@ -2,60 +2,66 @@ import ActivityCard from "../components/activityCard";
 import TodaysDate from "../components/todaysDate";
 import "../Style.css";
 
-import ProvaImmagine from "../assets/react.svg";
+// import ProvaImmagine from "../assets/react.svg";
 import PetTherapy from "../assets/IMG_3614.png";
 
 const Activities = () => {
-    return (
-        <>
-            <TodaysDate />
-            <div className="activities-container">
-                <div className="leftColumnActivities">
-                    <ActivityCard
-                        imageSrc={PetTherapy}
-                        title="Pet Therapy"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
-                    />
-                    <ActivityCard
-                        imageSrc={ProvaImmagine}
-                        title="Esempio di prova"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet,"
-                    />
-                    <ActivityCard
-                        imageSrc={ProvaImmagine}
-                        title="Esempio di prova"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet, bibendum non venenatis nisl tempor. Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit ame
-                bibendum non venenatis nisl tempor."
-                    />
-                </div>
+  return (
+    <>
+      <TodaysDate />
+      <div className="activities-container">
+        <div className="leftColumnActivities">
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Sempre"
+            dateAndTimeBovolone="Sempre"
+            description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
 
-                <div className="rightColumnActivities">
-                    <ActivityCard
-                        imageSrc={ProvaImmagine}
-                        title="Esempio di prova"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet,"
-                    />
-                    <ActivityCard
-                        imageSrc={ProvaImmagine}
-                        title="Esempio di prova"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet,"
-                    />
-                    <ActivityCard
-                        imageSrc={ProvaImmagine}
-                        title="Esempio di prova"
-                        dateAndTime="Sempre"
-                        description="Lorem ipsum dolor sit amet,
-                bibendum non venenatis nisl tempor. Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
-                    />
-                </div>
-            </div>
-        </>
-    );
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Concamarise giovedì 9:30/11:30"
+            dateAndTimeBovolone="Bovolone 9:30/11:30"
+            description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
+
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Sempre"
+            dateAndTimeBovolone="Sempre"
+            description="Lorem ipsum dolor sit amet, \nLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
+        </div>
+
+        <div className="rightColumnActivities">
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Sempre"
+            dateAndTimeBovolone="Sempre"
+            description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Sempre"
+            dateAndTimeBovolone="Sempre"
+            description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
+          <ActivityCard
+            imageSrc={PetTherapy}
+            title="Pet Therapy"
+            dateAndTimeConcamarise="Sempre"
+            dateAndTimeBovolone="Sempre"
+            description="Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet"
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Activities;
