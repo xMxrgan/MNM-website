63a0632d9099d12cc4764c0780d9969cf9b93676
xMxrgan <morganpicariello@posteo.net>
2025-05-12 12:15:52 +0200

    Changed some assets name

---
+++ src/views/DChome.tsx
@@ -0,0 +1,15 @@
+import DCnavbar from "../components/DCnavbar";
+import "../Style.css";
+
+const DailyCenter = () => {
+    return (
+        <>
+            <div>
+                <DCnavbar />
+                <h1>Torna più tardi...</h1>
+            </div>
+        </>
+    );
+};
+
+export default DailyCenter;
