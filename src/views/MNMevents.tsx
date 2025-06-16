34afac4095cbd6ba3417c2e1405d7beafd998805
xMxrgan <morganpicariello@posteo.net>
2025-06-16 19:09:10 +0200

    Update event carousel titles with specific event names

--- src/views/MNMevents.tsx
+++ src/views/MNMevents.tsx
@@ -53,159 +53,279 @@ const Eventi = () => {
             <div className="year">Anno 2025</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2025} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2025}
+                        title="Il tempo cammina i suoi passi"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2025} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2025}
+                        title="Centenario della scuola d'infanzia di Concamarise"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2025} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2025}
+                        title="Donne in musica e poesia"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2025} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2025}
+                        title="Carneval e tradizion"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2024</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc06_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc06_2024}
+                        title="Mercatini di Natale"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc05_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc05_2024}
+                        title="Festa di metà estate"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2024}
+                        title="Centro estivo"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2024}
+                        title="Note di femminilità"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2024}
+                        title="La festa dei nonni, super nonni!"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2024} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2024}
+                        title="Chiesa Antica di Concamarise in festa"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2023</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc11_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc11_2023}
+                        title="Un'appuntamento imperdibile"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc10_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc10_2023}
+                        title="Libiam, è Natal"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc09_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc09_2023}
+                        title="E poi sapete come andò a finire"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc08_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc08_2023}
+                        title="Ricorda chi si dimentica"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc07_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc07_2023}
+                        title="Meravigliosi anni '80"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc06_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc06_2023}
+                        title="Fine estate party"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc05_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc05_2023}
+                        title="ConcHistioria"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2023}
+                        title="Melodie in fiore"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2023}
+                        title="Libiam, è Natal"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2023}
+                        title="Invocando l'aurora"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2023} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2023}
+                        title="Creiamo insieme"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2022</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2022} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2022}
+                        title="La festa dei nonni, super nonni!"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2021</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2021} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2021}
+                        title="Creiamo insieme"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2021} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2021}
+                        title="Scambiamoci il nostro tempo"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2020</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2020} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2020}
+                        title="Serate di ballo e animazione"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2019</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc06_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc06_2019}
+                        title="Ti vengo a trovare"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc05_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc05_2019}
+                        title="Tombola di Natale"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2019}
+                        title="Na molonara fora dal comune"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2019}
+                        title="Canti in villa"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2019}
+                        title="Racconti di vita, un percorso autobiografico di storie e tradizioni"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2019} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2019}
+                        title="Giornata della memoria"
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2018</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2018} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2018}
+                        title="Un sogno nel cuore"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2018} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2018}
+                        title="Nonni in festa, viva i nonni!"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2018} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2018}
+                        title="Il tempo cammina i suoi passi"
+                    />
                 </div>
 
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2018} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2018}
+                        title="Mi na olta..."
+                    />
                 </div>
             </div>
 
             <div className="year">Anno 2017</div>
             <div className="mnm-events-carousel-wrapper">
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc05_2017} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc05_2017}
+                        title="Silenziose armonie"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc04_2017} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc04_2017}
+                        title="Inaugurazione del 'Centro Sollievo' di Bovolone"
+                    />
                 </div>
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc03_2017} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc03_2017}
+                        title="Sentire, vedere, toccare, gustare"
+                    />
                 </div>
 
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc02_2017} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc02_2017}
+                        title="Festa dei nonni"
+                    />
                 </div>
 
                 <div className="mnm-events-carousel-slide">
-                    <MNMeventsCarousel image={Loc01_2017} title="Aperitivi" />
+                    <MNMeventsCarousel
+                        image={Loc01_2017}
+                        title="La musica più bella dagli anni '30 agli anni '70"
+                    />
                 </div>
             </div>
         </>
